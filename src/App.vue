98756b09848995b56a2408cdf9532a36abff7412
ben <benfd@outlook.com>
2020-08-25 20:57:10 +0100

    MVP Complete

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,97 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+
+    <h1>Faker</h1>
+
+    <p>Total likes: {{ totalLikes }}</p>
+
+    <div class="flex">
+      
+      <div class="w-40"></div>
+
+      <div class="w-60">
+        <ul>
+          <tweet-list-item v-for="(tweet, index) in tweets" :key="index" :data="tweet"></tweet-list-item>
+        </ul>
+      </div>
+
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// Import
+import tweetListItem from './components/tweetListItem';
 
+// Create app component
 export default {
-  name: 'App',
+  name: 'app',
+  data() {
+    return {
+      tweets:[
+        {
+          id: 1,
+          name: 'James',
+          handle: '@jokerjames',
+          img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+          tweet: "If you don't succeed, dust yourself off and try again.",
+          likes: 10,
+        },
+        {
+          id: 2,
+          name: 'Fatima',
+          handle: '@fantasticfatima',
+          img: 'https://semantic-ui.com/images/avatar2/large/molly.png',
+          tweet: 'Better late than never but never late is better.',
+          likes: 12,
+        },
+        {
+          id: 3,
+          name: 'Xin',
+          handle: '@xeroxin',
+          img: 'https://semantic-ui.com/images/avatar2/large/elyse.png',
+          tweet: 'Beauty in the struggle, ugliness in the success.',
+          likes: 18,
+        }
+      ]
+    }
+  },
+  computed: {
+    totalLikes(){
+      return this.tweets.reduce((total, tweet) => total + tweet.likes, 0);
+    }
+  },
   components: {
-    HelloWorld
+    'tweet-list-item': tweetListItem
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+body{
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: 'Alata', sans-serif;
+}
+ul{
+  margin: 0;
+  padding: 0;
+  list-style-type: none;
+}
+
+.flex{
+    display: flex;
+}
+.w-20{
+    width: 20%;
+}
+.w-40{
+  width: 40%;
+}
+.w-60{
+  width: 60%;
+}
+.w-80{
+    width: 80%;
 }
 </style>
---
+++ src/components/tweetListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="tweet-layout">
+      <div class="flex">
+          <div class="w-20">
+              <img :src="data.img" />
+          </div>
+          <div class="w-80">
+              <p>{{ data.name }} | {{ data.likes }} LIKES</p>
+              <p>{{ data.handle }}</p>
+              <p>{{ data.tweet }}</p>
+          </div>
+      </div>   
+  </li>
+</template>
+
+<script>
+export default {
+    name: 'tweetListItem',
+    props: ['data']
+}
+</script>
+
+<style scoped>
+.tweet-layout{
+    margin: 10px auto;
+    background: rgb(228, 228, 228);
+}
+img{
+    width: 100%;
+}
+</style>
