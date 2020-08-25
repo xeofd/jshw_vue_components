a96d3bb2ad434bb5b08dd3e40d1d0b11e70da4f1
ben <benfd@outlook.com>
2020-08-25 21:51:50 +0100

    Extension done

--- src/App.vue
+++ src/App.vue
@@ -7,11 +7,41 @@
 
     <div class="flex">
       
-      <div class="w-40"></div>
+      <div class="w-40 form">
+
+        <h2>Filter</h2>
+
+        <input type="number" v-model.number="filterCount" placeholder="Like count" />
+
+        <h2>Add a tweet</h2>
+
+        <form v-on:submit.prevent="addNewTweet">
+
+          <label for="tweet_name">Name:</label>
+          <input type="text" id="tweet_name" v-model="newTweet.name" placeholder="Name" />
+          <br />
+
+          <label for="tweet_handle">Handle:</label>
+          <input type="text" id="tweet_handle" v-model="newTweet.handle" placeholder="Handle" />
+          <br />
+
+          <label for="tweet_handle">Tweet:</label>
+          <input type="text" id="tweet_text" v-model="newTweet.tweet" placeholder="Tweet text" />
+          <br />
+
+          <label for="tweet_handle">Likes:</label>
+          <input type="number" id="tweet_text" v-model.number="newTweet.likes" placeholder="Likes" min="0"/>
+          <br />
+
+          <input type="submit" value="Add tweet" />
+
+        </form>
+
+      </div>
 
       <div class="w-60">
         <ul>
-          <tweet-list-item v-for="(tweet, index) in tweets" :key="index" :data="tweet"></tweet-list-item>
+          <tweet-list-item v-for="(tweet, index) in filterTweets" :key="index" :data="tweet"></tweet-list-item>
         </ul>
       </div>
 
@@ -54,12 +84,39 @@ export default {
           tweet: 'Beauty in the struggle, ugliness in the success.',
           likes: 18,
         }
-      ]
+      ],
+      newTweet: {
+        id: null,
+        name: '',
+        handle: '',
+        img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+        tweet: '',
+        likes: 0
+      },
+      filterCount: 0
     }
   },
   computed: {
-    totalLikes(){
+    totalLikes: function(){
       return this.tweets.reduce((total, tweet) => total + tweet.likes, 0);
+    },
+    filterTweets: function(){
+      return this.tweets.filter((tweet) => tweet.likes >= this.filterCount);
+    }
+  },
+  methods: {
+    addNewTweet: function(){
+      this.newTweet.id = this.tweets.length + 1;
+      this.tweets.push(this.newTweet);
+
+      this.newTweet = {
+        id: null,
+        name: '',
+        handle: '',
+        img: 'https://semantic-ui.com/images/avatar2/large/matthew.png',
+        tweets: '',
+        likes: 0
+      }
     }
   },
   components: {
@@ -69,18 +126,24 @@ export default {
 </script>
 
 <style>
+*{
+  font-family: 'Alata', sans-serif;
+}
 body{
   text-align: center;
-  font-family: 'Alata', sans-serif;
 }
 ul{
   margin: 0;
   padding: 0;
   list-style-type: none;
 }
+h1, h2{
+  color: rgb(29,161,242);
+}
 
 .flex{
     display: flex;
+    align-items: flex-start;
 }
 .w-20{
     width: 20%;
@@ -93,5 +156,31 @@ ul{
 }
 .w-80{
     width: 80%;
+}
+
+.form{
+  margin-right: 20px;
+  background: rgb(225,232,237);
+}
+label{
+  width: 20%;
+}
+input{
+  margin: 10px;
+  padding: 10px 5px;
+  width: 80%;
+  border: 3px solid transparent;
+}
+input[type=submit]{
+  background: rgb(29,161,242);
+  color: rgb(255, 255, 255);
+}
+
+input:focus{
+  outline: none;
+  border: 3px solid rgb(29,161,242);
+}
+input[type=submit]:hover{
+  cursor: pointer;
 }
 </style>
--- src/components/tweetListItem.vue
+++ src/components/tweetListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="tweet-layout">
+  <li :class="listItemClass">
       <div class="flex">
           <div class="w-20">
               <img :src="data.img" />
@@ -16,16 +16,30 @@
 <script>
 export default {
     name: 'tweetListItem',
-    props: ['data']
+    props: ['data'],
+    computed: {
+        listItemClass: function(){
+            if (this.data.likes > 10){
+                return 'tweet-layout highlighted';
+            } else{
+                return 'tweet-layout';
+            }
+        }
+    }
 }
 </script>
 
 <style scoped>
 .tweet-layout{
     margin: 10px auto;
-    background: rgb(228, 228, 228);
+    background: rgb(245,248,250);
 }
 img{
     width: 100%;
+}
+
+.highlighted{
+    background: rgb(29,161,242);
+    color: rgb(255, 255, 255);
 }
 </style>
